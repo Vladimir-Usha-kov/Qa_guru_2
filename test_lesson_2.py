dd79ac3f26e669e3d952e57c1c9ab458e3176652
Vladimir <ushakov.vladimir98@mail.ru>
2023-06-09 00:07:49 +0400

    Домашнее задание урока 2 (2.0)

--- test_lesson_2.py
+++ test_lesson_2.py
@@ -4,22 +4,22 @@ from selene.support.conditions import have, be
 
 
 
-@pytest.fixture
+@pytest.fixture(scope= 'session', autouse= True)
 def size_window():
-    browser.config.window_width = 800
+    browser.config.window_width = 80
     browser.config.window_height = 1920
 
 
 
 
-def test_positive(size_window):
+def test_positive():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
 
-def test_negative(size_window):
+def test_negative():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('dfhgydfhrfgh').press_enter()
     browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
