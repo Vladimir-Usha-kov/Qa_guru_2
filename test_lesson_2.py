6722f6f810db88e732d39d1e1ef016738b40b7af
Vladimir <ushakov.vladimir98@mail.ru>
2023-06-09 11:43:15 +0400

    Фикс замечаний (отформатировал код)

--- test_lesson_2.py
+++ test_lesson_2.py
@@ -3,20 +3,16 @@ from selene import browser
 from selene.support.conditions import have, be
 
 
-
-@pytest.fixture(scope= 'session', autouse= True)
+@pytest.fixture(scope='session', autouse=True)
 def size_window():
     browser.config.window_width = 800
     browser.config.window_height = 1920
-
-
 
 
 def test_positive():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
-
 
 
 def test_negative():
